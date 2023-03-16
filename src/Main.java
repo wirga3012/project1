208f713339ec82243f139f567310dc0d6a7860ac
Nerijus Marcinkenas <nerijus.marcinkenas@gmail.com>
2023-03-16 22:14:04 +0200

    tasks 1-10

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
-import org.w3c.dom.ls.LSOutput;
-
-import java.sql.SQLOutput;
-
 public class Main {
     public static void main(String[] args) {
-
-
-
         /*
         1.Užduotis. Sukurti du kintamuosius. Jiems priskirti savo mylimo aktoriaus vardą ir pavardę kaip stringus
         (Jonas Jonaitis). Atspausdinti trumpesnį stringą.
@@ -116,102 +109,117 @@ public class Main {
 
         System.out.println("MASYVAI");
 
-        {int num [] = {1,4,6,0,4,8};
-            System.out.println(num [0]);
-            System.out.println(num [1]);
-            System.out.println(num [2]);
-            System.out.println(num [3]);
-            System.out.println(num [4]);
-            System.out.println(num [5]);
+        int[] num = {1, 4, 6, 0, 4, 8};
+        System.out.println(num[0]);
+        System.out.println(num[1]);
+        System.out.println(num[2]);
+        System.out.println(num[3]);
+        System.out.println(num[4]);
+        System.out.println(num[5]);
 
 
+        int[][] array2d = {{1, 2, 3}, {4, 5, 6},};
 
-        {int [][]array2d = {
-                {1,2,3},
-                {4,5,6},
-
-        };
-            System.out.println(array2d[0][0] + "," + array2d[0][1] + "," + array2d[0][2]);
-            System.out.println(array2d[1][0]);
+        System.out.println(array2d[0][0] + "," + array2d[0][1] + "," + array2d[0][2]);
+        System.out.println(array2d[1][0]);
 
 
 //            Sukurkite ciklą kuris atspausdintų 10 kartų žodį “labas”.
-            System.out.println("-----1 užduotis------");
-            for (int i = 0; i <10; i++ ){
-                System.out.println("labas");
-            }
-//            Sukurkite ciklą kuris atspausdintų skaičius nuo 0 iki 9.
-            System.out.println("-----2 užduotis------");
-            for (int i = 0; i < 10 ; i++ ){
-                System.out.println(i);
-            }
-//            Sukurkite masyvą su dešimties augalų pavadinimų.
-            System.out.println("-----3 užduotis------");
+        System.out.println("-----1 užduotis------");
 
-            String [] plantName = {"aguona","ramune" ,"bijūnas","rožė","erškėtis","raktažolė", "puriena", "tulpė","pakalnutė","rozmarinas"};
-            for (int i = 0; i < plantName.length; i++){
+        for (int i = 0; i < 10; i++) {
+            System.out.println("labas");
+        }
+//            Sukurkite ciklą kuris atspausdintų skaičius nuo 0 iki 9.
+        System.out.println("-----2 užduotis------");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+//            Sukurkite masyvą su dešimties augalų pavadinimų.
+        System.out.println("-----3 užduotis------");
+
+        String[] plantNames = {"aguona", "ramune", "bijūnas", "rožė", "erškėtis", "raktažolė", "puriena", "tulpė", "pakalnutė", "rozmarinas"};
+//        Atspausdinkite kiekvieną 3čio uždavinio augalą atskiroje eilutėje.
+
+        System.out.println("-----4 užduotis------");
+        for (int i = 0; i < plantNames.length; i++) {
 //
-                System.out.println(plantName[i]);
-            }
+            System.out.println(plantNames[i]);
+        }
 //           Atspausdinkite 3čio uždavinio kiekvieną augalą pradedant nuo paskutinio, ir baigiant pirmuoju. (atvirkščias ciklas).
 
-            System.out.println("-----5 Užduotis----------");
-            for (int i = plantName.length - 1; i >= 0 ; i--) {
-                System.out.println(plantName[i]);
-            }
+        System.out.println("-----5 Užduotis----------");
+        for (int i = plantNames.length - 1; i >= 0; i--) {
+            System.out.println(plantNames[i]);
+        }
 
 //            Atspausdinkite kas antrą skaičių nuo 10 iki 50 (porinius)
 
-            System.out.println("----6 Užduotis--------------");
-            System.out.println("----7 Užduotis--------------");
-            System.out.println("----8 Užduotis--------------");
-            System.out.println("----9 Užduotis--------------");
-            System.out.println("----10 Užduotis-------------");
+        System.out.println("----6 Užduotis--------------");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        for (int i = 10; i <= 50; i += 2) {
+            System.out.println(i);
 
         }
+//        Atspausdinkite kas antrą skaičių nuo 10 iki 50. (pornius) Jei skaičius dalinasi iš 10 be liekanos jo nespausdinkite.
+//        ( naudokite continue.) (atspausdinti visus porinus skaičius, išskyrus tuos kurie dalinasi iš 10 be liekanos)
 
+        System.out.println("----7 Užduotis--------------");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        for (int i = 10; i <= 50 ; i++) {
+            if (i % 10 == 0){
+                continue;
+            }
+            System.out.println(i);
 
         }
+//        Sukurkite ciklą kuris suktųsi nuo 0 iki 20. Suskaičiuokite, kiek kartų kintamasis i turėjo porinę reikšmę;
 
+        System.out.println("----8 Užduotis--------------");
+        int sum = 0;
+        for (int i = 0; i < 20; i++) {
+            if (i % 2 == 0){
+                sum++;
+            }
+        }
+        System.out.println(sum);
+        //Suskaičiuokite kiek 3čio uždavinio masyve yra žodžių trumpesnių nei 5 simboliai, ir kiek ilgesnių nei 7 simboliai. (du skaičiavimai)
 
+        System.out.println("----9 Užduotis--------------");
 
+        int shortNamesCount = 0;
+        int longNamesCount = 0;
+
+        for (int i = 0; i < plantNames.length; i++) {
+            if (plantNames[i].length() < 5){
+                shortNamesCount++;
+            }
+            if (plantNames[i].length() > 7){
+                longNamesCount++;
+            }
+        }
+
+        System.out.println("trumpesni simboliai- " + shortNamesCount + " ilgesni simboliai" + longNamesCount);
+
+//      Suskaičiuokite kiek 3čio uždavinio masyve yra žodžių ilgesnių nei 5 simboliai bet trumpesnių  nei 10 simboliai. (tarp 5 ir 10 simbolių ilgio)
+
+        System.out.println("----10 Užduotis-------------");
+
+        int between = 0;
+
+        for (int i = 0; i < plantNames.length; i++) {
+            if (plantNames[i].length()> 5 && plantNames[i].length() < 10){
+                between++;
+            }
+        }
+        System.out.println(between);
+
+//
+//        String[] plantNames2 = {"aguona", "ramune", "bijūnas", "rožė", "erškėtis", "raktažolė", "puriena", "tulpė", "pakalnutė", "rozmarinas"};
+//        for (int i = plantNames2.length - 1; i >= 0; i--) {
+//            System.out.println(plantNames2[i]);
+
+//        }
 
 
 
